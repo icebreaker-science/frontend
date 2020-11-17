7a3d169936600bd772d23f13ed3fd075c660eda2
Daniel <daniel@jenet.xyz>
2020-11-17 20:41:50 +0100

    add hCaptcha for registration and contact

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   backendUrl: '/api',
   maxImageSizeKB: 1024,
-  allowedImageTypes: 'image/jpeg,image/bmp,image/png'
+  allowedImageTypes: 'image/jpeg,image/bmp,image/png',
+  captchaSiteKey: '10000000-ffff-ffff-ffff-000000000001'
 };
