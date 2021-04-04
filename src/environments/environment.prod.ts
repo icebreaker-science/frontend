00542907d11912fbc23d97ab867bcbe506e4d659
Chaoran Chen <mail@chaoran-chen.de>
2021-04-05 00:35:02 +0200

    Add production hcaptcha site key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   backendUrl: '/api',
   maxImageSizeKB: 1024,
   allowedImageTypes: 'image/jpeg,image/bmp,image/png',
-  captchaSiteKey: '10000000-ffff-ffff-ffff-000000000001'
+  captchaSiteKey: 'b627f11c-73d8-422a-aff7-67e4fcbc5044'
 };
